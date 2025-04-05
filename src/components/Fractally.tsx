a9a360f949f7c0a388971f052ef4de31b38640ed
zzzzBov <zzzzbov@gmail.com>
2025-04-04 23:01:35 -0400

    Create container components

--- src/components/Fractally.tsx
+++ src/components/Fractally.tsx
@@ -1,3 +1,27 @@
+import { PropsWithChildren } from "react";
+import style from "./Fractally.module.scss";
+
 export function Fractally() {
-  return <>Hello, World!</>;
+  return (
+    <Container>
+      <Canvas>{/* TODO */}</Canvas>
+      <Controls>{/* TODO */}</Controls>
+    </Container>
+  );
+}
+
+function Container({ children }: PropsWithChildren) {
+  return <div className={style.container}>{children}</div>;
+}
+
+export function Canvas({ children }: PropsWithChildren) {
+  return (
+    <svg className={style.canvas} viewBox="0 0 16 16">
+      {children}
+    </svg>
+  );
+}
+
+export function Controls({ children }: PropsWithChildren) {
+  return <div className={style.controls}>{children}</div>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Fractally } from "@/components/Fractally";
+import "@/styles/styles.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
