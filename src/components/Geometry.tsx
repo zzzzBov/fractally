8680823d3d6dfb6fca12c16f0b02975cc43349b3
zzzzBov <zzzzbov@gmail.com>
2025-04-04 23:42:41 -0400

    Added gripline

--- src/components/Geometry.tsx
+++ src/components/Geometry.tsx
@@ -9,5 +9,13 @@ export function DerivedLines() {
 }
 
 export function GripLine() {
-  return <></>;
+  return (
+    <g>
+      <line className={style.gripline} x1="12.2" y1="7.4" x2="6.9" y2="2.1" />
+      <circle className={style.touch} cx="12.2" cy="7.4" r="1" />
+      <circle className={style.touch} cx="6.9" cy="2.1" r="1" />
+      <circle className={style.grip} cx="12.2" cy="7.4" r="0.12" />
+      <circle className={style.grip} cx="6.9" cy="2.1" r="0.12" />
+    </g>
+  );
 }
